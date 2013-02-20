d3e2e705c8eb003936ebde50d509d202d4af9257
Jesse Szwedko <j.szwedko@modcloth.com>
2013-02-20 14:53:41 -0500

    Adding amqp-tools cookbook
    Signed-off-by: Rafe Colton <r.colton@modcloth.com>

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'amqp-tools'
+maintainer       'ModCloth, Inc.'
+maintainer_email 'external.tools@modcloth.com'
+license          'All rights reserved'
+description      'Installs/Configures amqp-tools'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ recipes/base.rb
@@ -0,0 +1,24 @@
+package 'golang'
+
+bash 'build' do
+  environment(
+    'PATH' => %w{
+        /opt/local/bin
+        /opt/local/sbin
+        /usr/bin
+        /usr/sbin
+        /bin
+        /sbin
+    }.join(':')
+  )
+  code <<-EOB
+      set -e
+      export GOPATH=$(go env GOROOT)
+      go get -x -u github.com/modcloth/amqp-tools
+      go install -x github.com/modcloth/amqp-tools/amqp-consume-cat
+      go install -x github.com/modcloth/amqp-tools/amqp-publish-files
+      ln -sv $GOPATH/bin/amqp-consume-cat #{node['install_prefix']}/bin/amqp-consume-cat
+      ln -sv $GOPATH/bin/amqp-publish-files #{node['install_prefix']}/bin/amqp-publish-files
+      chmod 0755 $GOPATH/bin/amqp-{consume-cat,publish-files}
+  EOB
+end
