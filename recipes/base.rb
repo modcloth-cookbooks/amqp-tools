185683a4516574623b89a735824d546cb203d747
Sheena McCoy <sp.mccoy@modcloth.com>
2013-02-21 17:43:53 -0500

    Force symlink of files
    To correct failure after initial deploy
    
    Signed-off-by: Rachel Thompson <r.thompson@modcloth.com>

--- recipes/base.rb
+++ recipes/base.rb
@@ -17,8 +17,8 @@ bash 'build' do
       go get -x -u github.com/modcloth/amqp-tools
       go install -x github.com/modcloth/amqp-tools/amqp-consume-cat
       go install -x github.com/modcloth/amqp-tools/amqp-publish-files
-      ln -sv $GOPATH/bin/amqp-consume-cat #{node['install_prefix']}/bin/amqp-consume-cat
-      ln -sv $GOPATH/bin/amqp-publish-files #{node['install_prefix']}/bin/amqp-publish-files
+      ln -svf $GOPATH/bin/amqp-consume-cat #{node['install_prefix']}/bin/amqp-consume-cat
+      ln -svf $GOPATH/bin/amqp-publish-files #{node['install_prefix']}/bin/amqp-publish-files
       chmod 0755 $GOPATH/bin/amqp-{consume-cat,publish-files}
   EOB
 end
